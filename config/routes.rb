b1acaf4fb8fb30fae05758110c04c6ee01b783c4
Prashant Angyan <prashant@itxsolutionsindia.com>
2012-01-05 13:35:07 +0530

    Before upgrading the rails code to the new code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Myteeroom::Application.routes.draw do
-  #get "home/index"
+  get "home/index"
 
-  #resources :items
+  resources :items
 
-  #get "about/index"
+  get "about/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,7 +55,7 @@ Myteeroom::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
- #root :to => 'home#index'
+ 		root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
